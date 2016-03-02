82f74449a1f1316be46199dd083a37ea9226d1a5
323096628 <323096628@C-3ZZHYR1.tdsb.on.ca>
2016-03-02 14:44:47 -0500

    This program says "Hello World" when it is run. It is my first program made using this format.

---
+++ ICS3U/src/christopherakos/HelloWorld.java
@@ -0,0 +1,20 @@
+package christopherakos;
+
+
+
+/*
+ * helloworld.java
+ * This program prints Hello World to the console
+ * Peter Christopherakos
+ * March 2, 2016
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		//This prints "Hello World" to the console
+		System.out.println("Hello World");
+
+	}
+
+}
+
